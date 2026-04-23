9d2219455d514b6fdb5743a225834b7ebd95c7e1
olirvs <oliviamazone@gmail.com>
2026-04-23 17:51:52 +0200

    Complete Class Presence.java

--- src/main/java/Entites/Presence.java
+++ src/main/java/Entites/Presence.java
@@ -1,4 +1,12 @@
 package Entites;
 
+import jakarta.persistence.Id;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.lang.annotation.Inherited;
+
 public class Presence {
+    @Id
+    public Long Id;
+
 }
