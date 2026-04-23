3e71db42f4be2a279ff1a448da79c5781f0e01ec
olirvs <oliviamazone@gmail.com>
2026-04-23 16:47:55 +0200

    test

---
+++ src/test/java/org/example/odoru/OdoruApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.odoru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdoruApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
