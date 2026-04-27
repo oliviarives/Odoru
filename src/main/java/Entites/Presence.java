2797143a2b03c6a08f061910b0a746f5007e9e63
olirvs <oliviamazone@gmail.com>
2026-04-27 10:27:04 +0200

    Ajout variable DateHeure Presence.java

--- src/main/java/Entites/Presence.java
+++ src/main/java/Entites/Presence.java
@@ -4,9 +4,20 @@ import jakarta.persistence.Id;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.lang.annotation.Inherited;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class Presence {
     @Id
     public Long Id;
 
+    /*@DateTimeFormat(pattern = "yyyy-MM-dd")
+    LocalDate date;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
+    LocalTime time;*/
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    LocalDateTime dateTime;
 }
